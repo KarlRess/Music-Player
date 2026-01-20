e2a44ddebe56196b981eaf969dc3c86bd7d935d3
Indriyan Respati <indriyanattar@gmail.com>
2026-01-20 21:54:26 +0700

    style Category

--- src/components/Main/Category.tsx
+++ src/components/Main/Category.tsx
@@ -1,6 +1,7 @@
 import SongCard from "./SongCard.tsx";
 import type { Song } from "../../data/song.ts";
 import { useRef } from "react";
+import { leftArrowIcon, rightArrowIcon } from "../assets.ts";
 
 const Category = ({ category, song }: { category: string; song: Song[] }) => {
   const track = useRef<HTMLDivElement | null>(null);
@@ -18,29 +19,29 @@ const Category = ({ category, song }: { category: string; song: Song[] }) => {
   return (
     <div className="group flex flex-col gap-1">
       <div className="flex items-center justify-between pr-2">
-        <div className="pl-7 text-xl font-semibold">{category}</div>
+        <div className="pl-8 text-2xl font-semibold">{category}</div>
 
         {/* Carousel Button */}
-        <div className="bg-layout flex h-full items-center gap-1 p-1 opacity-0 transition-opacity duration-500 group-hover:opacity-100">
+        <div className="flex h-full items-center gap-3 p-1 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
           <button
             onClick={() => handleScroll("left")}
-            className="bg-surface-secondary flex-center h-6 w-6 cursor-pointer rounded-full"
+            className="bg-surface-secondary flex-center h-8 w-8 cursor-pointer rounded-full pr-0.5 shadow-md shadow-black/20 hover:brightness-125"
           >
-            L
+            <img src={leftArrowIcon} alt="previous slide" />
           </button>
           <button
             onClick={() => handleScroll("right")}
-            className="bg-surface-secondary flex-center h-6 w-6 cursor-pointer rounded-full"
+            className="bg-surface-secondary flex-center h-8 w-8 cursor-pointer rounded-full pl-0.5 shadow-md shadow-black/20 hover:brightness-125"
           >
-            R
+            <img src={rightArrowIcon} alt="next slide" />
           </button>
         </div>
       </div>
 
       <div className="flex items-start overflow-hidden pl-5" ref={track}>
         {/* Music Card */}
-        {song.map((s, i) => (
-          <SongCard key={s.id} song={s} index={i} />
+        {song.map((s) => (
+          <SongCard key={s.id} song={s} />
         ))}
       </div>
     </div>
--- src/components/assets.ts
+++ src/components/assets.ts
@@ -3,9 +3,13 @@ import history from "../assets/Icons/history_icon.svg";
 import pict from "../assets/User-Picture/user_picture.png";
 import smallPlay from "../assets/Icons/small_play_icon.svg";
 import smallPause from "../assets/Icons/small_pause_icon.svg";
+import leftArrow from "../assets/Icons/arrow_left_icon.svg";
+import rightArrow from "../assets/Icons/arrow_right_icon.svg";
 
 export const searchIcon = search;
 export const userPict = pict;
 export const historyIcon = history;
 export const smallPlayIcon = smallPlay;
 export const smallPauseIcon = smallPause;
+export const leftArrowIcon = leftArrow;
+export const rightArrowIcon = rightArrow;
