e055378dd6209a4df8002eff7938970c475fda58
Indriyan Respati <indriyanattar@gmail.com>
2026-01-20 21:58:41 +0700

    add song

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,7 +1,7 @@
 import { song } from "../../data/song.ts";
 import Category from "./Category.tsx";
 
-const category: string[] = ["Hip-Hop", "Pop", "Blues"];
+const category: string[] = ["Hip-Hop", "Pop", "Indie", "Blues"];
 
 const Main = () => {
   return (
